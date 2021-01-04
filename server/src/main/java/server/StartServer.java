2d481d10f42c750a85bfda3afc4bcf1984dd2fcb
ViktorKov <kovalskij8@yandex.ru>
2021-01-04 16:52:24 +0300

    Дз урок 6

--- server/src/main/java/server/StartServer.java
+++ server/src/main/java/server/StartServer.java
@@ -10,7 +10,7 @@ public class StartServer {
     private static final Logger logger = Logger.getLogger("");
     public static void main(String[] args) {
         try {
-            Handler fileHandler = new FileHandler("log%_g.txt",20 * 1025, 10, true);
+            Handler fileHandler = new FileHandler("log_%g.txt",20 * 1025, 10, true);
             fileHandler.setFormatter(new SimpleFormatter());
             logger.addHandler(fileHandler);
         } catch (IOException e) {
